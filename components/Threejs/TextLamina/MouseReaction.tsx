46bcd05c1465c95f742b9682a928f04c76eab3a2
ypsokol <madnseries@gmail.com>
2022-06-16 20:17:58 +0300

    Add: Mouse event reaction

---
+++ components/Threejs/TextLamina/MouseReaction.tsx
@@ -0,0 +1,18 @@
+import * as THREE from "three";
+import { useFrame } from "@react-three/fiber";
+import { Vector3 } from "three";
+
+type Props = {
+  v: Vector3;
+};
+
+const MouseReaction = ({ v = new THREE.Vector3() }: Props) => {
+  return useFrame((state) => {
+    state.camera.position.lerp(
+      v.set(state.mouse.x / 2, state.mouse.y / 2, 10),
+      0.05
+    );
+  });
+};
+
+export default MouseReaction;
