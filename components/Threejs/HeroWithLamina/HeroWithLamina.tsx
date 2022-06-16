54c45aba87008592be4b747effc508d8c9b95b69
Yura Sokolov <madnseries@gmail.com>
2022-06-17 00:17:22 +0300

    Add: correct caption path

--- components/Threejs/HeroWithLamina/HeroWithLamina.tsx
+++ components/Threejs/HeroWithLamina/HeroWithLamina.tsx
@@ -1,23 +1,23 @@
 import { FC, Suspense } from "react";
 import MouseReaction from "../MouseReaction";
-import Caption from "../HeroWithFloaters/Caption";
+import Caption from "../Caption";
 import { Canvas } from "@react-three/fiber";
 import Background from "./Background";
 
 type Props = {
-  title: string;
+	title: string;
 };
 
 const HeroWithLamina = ({ title }: Props) => {
-  return (
-    <Canvas dpr={[1, 2]} camera={{ position: [0, 0, 10], fov: 22 }}>
-      <Background />
-      <MouseReaction />
-      <Suspense fallback={null}>
-        <Caption>{title}</Caption>
-      </Suspense>
-    </Canvas>
-  );
+	return (
+		<Canvas dpr={[1, 2]} camera={{ position: [0, 0, 10], fov: 22 }}>
+			<Background />
+			<MouseReaction />
+			<Suspense fallback={null}>
+				<Caption>{title}</Caption>
+			</Suspense>
+		</Canvas>
+	);
 };
 
 export default HeroWithLamina;
