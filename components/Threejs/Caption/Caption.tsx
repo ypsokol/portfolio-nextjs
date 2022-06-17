7f05262caa93c93762b97b0803b4c7f7d6f9db50
ypsokol <madnseries@gmail.com>
2022-06-17 08:52:38 +0300

    Add: missing fonts

--- components/Threejs/Caption/Caption.tsx
+++ components/Threejs/Caption/Caption.tsx
@@ -9,7 +9,7 @@ const Caption = ({ children }: PropsWithChildren) => {
     <Text
       position={[0, 0, -5]}
       lineHeight={0.8}
-      font="/Ki-Medium.ttf"
+      font="/fonts/WILDWORLD_FONT.otf"
       fontSize={width / 8}
       material-toneMapped={false}
       anchorX="center"
