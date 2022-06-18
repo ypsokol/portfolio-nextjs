875980fac12213ec065f03ff02f7b52b5bbc188d
ypsokol <madnseries@gmail.com>
2022-06-18 16:30:03 +0300

    Add: suspense fallback for threejs text

--- components/Threejs/HeroWithLamina/HeroWithLamina.tsx
+++ components/Threejs/HeroWithLamina/HeroWithLamina.tsx
@@ -1,8 +1,11 @@
 import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Html } from "@react-three/drei";
+
+import Background from "./Background";
 import MouseReaction from "../MouseReaction";
 import Caption from "../Caption";
-import { Canvas } from "@react-three/fiber";
-import Background from "./Background";
+import Loading from "../../UI/Loading";
 
 type Props = {
   title: string;
@@ -13,7 +16,13 @@ const HeroWithLamina = ({ title }: Props) => {
     <Canvas dpr={[1, 2]} camera={{ position: [0, 0, 10], fov: 22 }}>
       <Background />
       <MouseReaction />
-      <Suspense fallback={null}>
+      <Suspense
+        fallback={
+          <Html center>
+            <Loading variant="white" />
+          </Html>
+        }
+      >
         <Caption>{title}</Caption>
       </Suspense>
     </Canvas>
